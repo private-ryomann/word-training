e55b436f14d8d154294333f74eb4ac91e7387b5b
private-ryomann <ryomann46497@gmail.com>
2025-02-08 23:15:23 +0900

    DBスキーマ作成

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -1,5 +1,10 @@
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
+/**
+ * # 注意点
+ * - defaultFnは実行時にDrizzle側で生成されるので、データを入れる際はDrizzleを使うこと
+ *   @link(https://orm.drizzle.team/docs/column-types/pg#default-value)
+ */
 export const users = pgTable("user", {
 	id: text("id")
 		.primaryKey()
