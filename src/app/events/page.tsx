e5b3338b13fb2a1d50ba3ebe4835dd70d248ce1f
private-ryomann <ryomann46497@gmail.com>
2025-02-13 07:32:12 +0900

    page.tsx途中コミット

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -14,15 +14,20 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from "@/components/ui/drawer";
+import { db } from "@/db";
+import { eventTags } from "@/db/schemas/eventTags";
+import { events } from "@/db/schemas/events";
+import { tags } from "@/db/schemas/tags";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { format } from "date-fns";
+import { eq } from "drizzle-orm";
 import { FaCirclePlus } from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
 /**
  * イベント一覧表示画面
  */
 
-const tags = [
+const sampleTags = [
 	{
 		id: 1,
 		title: "タグ1",
@@ -40,7 +45,7 @@ const tags = [
 	},
 ];
 
-const events = [
+const eventSamples = [
 	{
 		id: 1,
 		title: "イベント1",
@@ -84,7 +89,22 @@ const events = [
 		tags: tags,
 	},
 ];
-export default function EventsPage() {
+export default async function EventsPage() {
+	const selectedEvents = await db
+		.select()
+		.from(events)
+		.innerJoin(eventTags, eq(eventTags.eventId, events.id))
+		.innerJoin(tags, eq(tags.id, eventTags.tagId))
+		.where(eq(events.userId, "9cebcb55-7b7c-4280-80f0-6f261c038bc1"));
+
+	const Events = await db.query.events.findMany({
+		with: {
+			tags: true,
+		},
+		where: (events, { eq }) =>
+			eq(events.userId, "9cebcb55-7b7c-4280-80f0-6f261c038bc1"),
+	});
+
 	return (
 		<>
 			<header className="h-10 bg-primary p-1">
@@ -111,7 +131,7 @@ export default function EventsPage() {
 			<main className="h-[calc(h-screen - h-10)] p-2">
 				<div className="flex flex-col">
 					<div className="flex flex-row">
-						{tags.map((tag) => (
+						{sampleTags.map((tag) => (
 							<Badge key={tag.id} className="mr-2">
 								{tag.title}
 							</Badge>
@@ -119,7 +139,7 @@ export default function EventsPage() {
 					</div>
 				</div>
 				<div className="flex flex-col mt-3">
-					{events.map((event) => (
+					{eventSamples.map((event) => (
 						<Card key={event.id} className="mt-3">
 							<CardHeader className="p-3">
 								<CardTitle>{event.title}</CardTitle>
