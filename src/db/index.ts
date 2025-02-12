f54db59ea55b1c466d354ca2afbb0ba96ad1d721
private-ryomann <ryomann46497@gmail.com>
2025-02-13 07:31:50 +0900

    スキーマをdrizzleインスタンスに入れる

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,9 @@
 import serverEnv from "@/env/serverEnv";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as eventTags from "./schemas/eventTags";
+import * as events from "./schemas/events";
+import * as tags from "./schemas/tags";
 
-export const db = drizzle(serverEnv.DATABASE_URL);
+export const db = drizzle(serverEnv.DATABASE_URL, {
+	schema: { ...events, ...tags, ...eventTags },
+});
